0b8161cf8efd586f35e865b110c78cb6f4a23310
thaiphamhuonggiang5148-cyber <thaiphamhuonggiang5148@gmail.com>
2026-04-13 11:04:16 +0700

    add mutiple files

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,20 @@
-import { useState } from "react";
+import React from "react";
 import { BrowserRouter } from "react-router-dom";
 import Header from "./components/Header/Header";
+import Footer from "./components/Footer/Footer";
 
 export default function App() {
-  const [page, setPage] = useState("login");
-
   return (
     <BrowserRouter>
-      <Header />
+      <div className="app-wrapper">
+        <Header />
+        
+        <main style={{ padding: "0" }}>
+          
+        </main>
+
+        <Footer />
+      </div>
     </BrowserRouter>
   );
 }
